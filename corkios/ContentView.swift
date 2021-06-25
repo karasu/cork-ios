660bc865e7a0128a4736f624c18db30f65431c9b
karasu <gustaucastells@gmail.com>
2021-06-25 11:04:04 +0200

    Initial Commit

---
+++ corkios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  corkios
+//
+//  Created by Gustau Castells on 25/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ corkios/corkiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  corkiosApp.swift
+//  corkios
+//
+//  Created by Gustau Castells on 25/6/21.
+//
+
+import SwiftUI
+
+@main
+struct corkiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
